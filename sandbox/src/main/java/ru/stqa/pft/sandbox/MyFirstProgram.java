ed11febed306fcc45bb529f516236a22045f5f8a
valentinkorneev <48074638+valentinkorneev@users.noreply.github.com>
2019-03-04 04:11:16 +0300

    Добавлен класс Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.sandbox;
 
-import java.sql.SQLOutput;
-
 public class MyFirstProgram {
 
 	public static void main(String[] args) {
@@ -10,23 +8,24 @@ public class MyFirstProgram {
 		hello("Jackass");
 
 		Square s = new Square(5);
-		s.l = 5;
-		System.out.println("Площадь квадрата со стороной " + s.l + " равна " + area(s));
+		System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
 		Rectangle r = new Rectangle(4, 6);
-		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + area(r));
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+		Point p1 = new Point(2,0);
+		Point p2 = new Point(6,0);
+
+		System.out.println("Расстояние между двумя точками p1 и p2 равно " + Point.distance(p1, p2));
+
 	}
 
 	public static void hello(String somebody) {
 		System.out.println("Hello, " + somebody +"!");
 	}
 
-	public static double area(Square s){
-		return s.l * s.l;
-	}
 
-	public static double area(Rectangle r) {
-		return r.a * r.b;
-	}
+
+
 
 }
