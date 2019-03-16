24acf90dad80c0ae0f06e93b93da94b6f67b7a33
valentinkorneev <48074638+valentinkorneev@users.noreply.github.com>
2019-03-17 01:57:53 +0300

    Helpers Added

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/test/GroupCreationTests.java
@@ -10,13 +10,13 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
 
-    app.wd.findElement(By.id("container")).click();
-    app.gotoGroupPage();
-    app.initGroupCreation("new");
-    app.fillGroupForm(new GroupData("test1", "test2", "test3"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
-    app.wd.findElement(By.linkText("Logout")).click();
+    app.getGroupHelper().wd.findElement(By.id("container")).click();
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().initGroupCreation("new");
+    app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().wd.findElement(By.linkText("Logout")).click();
   }
 
 
